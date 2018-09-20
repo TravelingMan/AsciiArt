ce19e1ccddc5efa16a22287e5395cfba504bb95e
S. Hall <shall.usnavy@gmail.com>
2018-09-20 08:55:39 -0400

    Initial logic. In need of a refactor.

---
+++ AsciiArt/Art.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsciiArt
+{
+    class Art
+    {
+        Bitmap bitmap;
+
+        public Art(string path)
+        {
+            // TODO Exception handling and basic sanity of IO
+            bitmap = new Bitmap(path);
+        }
+
+        public int[,] GetAverageBrightnessMatrix()
+        {
+            var colorMatrix = GetColorMatrix();
+            int[,] brightnessMatrix = new int[bitmap.Width, bitmap.Height];
+
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                for (int j = 0; j < bitmap.Height; j++)
+                {
+                    // Average the values for a simplified average brightness
+                    brightnessMatrix[i, j] = (
+                        bitmap.GetPixel(i, j).R + bitmap.GetPixel(i, j).G + bitmap.GetPixel(i, j).B
+                        ) / 3;
+                }
+            }
+
+            return brightnessMatrix;
+        }
+
+        // TODO: Strategy pattern?
+        private Color[,] GetColorMatrix()
+        {
+            Color[,] pixelMatrix = new Color[bitmap.Width, bitmap.Height];
+
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                for (int j = 0; j < bitmap.Height; j++)
+                {
+                    pixelMatrix[i, j] = bitmap.GetPixel(i, j);
+                }
+            }
+
+            return pixelMatrix;
+        }
+    }
+}
--- AsciiArt/Program.cs
+++ AsciiArt/Program.cs
@@ -10,6 +10,22 @@ namespace AsciiArt
     {
         static void Main(string[] args)
         {
+            var art = new Art("C:\\Geezus.jpg");
+
+            int[,] brightness = art.GetAverageBrightnessMatrix();
+            int count = 0;
+
+            for (int i = 0; i < brightness.GetLength(0); i++)
+            {
+                for (int j = 0; j < brightness.GetLength(1); j++)
+                {
+                    Console.WriteLine(brightness[i, j]);
+                    count++;
+                }
+            }
+            
+            Console.WriteLine("=======\nProcessed " + count + " pixels");
+            Console.ReadKey();
         }
     }
 }
